ce8e6af5045456bce8fa2c6f3ae2e0347618189d
dan <danfromboone@gmail.com>
2020-03-09 20:35:04 -0500

    enhanced install script. Handles installing Plug

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 ln -sf "$DIR/vimrc" ~/.vimrc
 
-
+vim -c PlugInstall -c q -c q
 
 
