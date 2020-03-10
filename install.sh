922c602edb9b8329b443673a243ba4a2d9294a5c
dan <danfromboone@gmail.com>
2020-03-09 20:37:32 -0500

    comments

--- install.sh
+++ install.sh
@@ -2,9 +2,13 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+# install Plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+#link vimrc
 ln -sf "$DIR/vimrc" ~/.vimrc
 
+#initialize Plug
 vim -c PlugInstall -c q -c q
 
 
